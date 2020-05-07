20174905e95f994d65d341d48dc233059527de21
brsgoc <nprobg@gmail.com>
2020-05-07 16:58:52 +0300

    added some modifications on tests.

--- src/test/testLogin.java
+++ src/test/testLogin.java
@@ -6,36 +6,24 @@ import com.company.database;
 import static com.company.RegisterScreen.*;
 
 public class testLogin {
-		String username="brsgoc";
-	    String password="1234";
-	    String name="baris";
-	    String surname="goc";
-	    int age=24;
-	    String gender="Male";
-	    String address="XXX";
 	    database d=new database();
-	    int id=1234;
 	    
 	@Test
 	public void testLoginDetails_Positive() {
-		d.registerCustomer(id,username, password, name, surname, age, gender, address);
 		//The user should be able to register.
-		assertTrue(d.CheckCustomerUserName(username)
-				&& d.CheckCustomerPassword(password));
+		d.registerCustomer(1234,"brsgoc","1234","baris","goc",24, "Male","xxx");
+		assertTrue(d.CheckCustomerUserName("brsgoc")&& d.CheckCustomerPassword("1234"));
 		d.registerEmployee(12,"kaan","abcst","kaan","gokcek",33,"male","xxx");
-		//The user should be able to register.
 		assertTrue(d.CheckEmployeeUserName("kaan")&& d.CheckEmployeePassword("abcst"));
 		}
 	@Test
 	public void testLoginDetails_Negative() {
-		String username="bgoc";String password="2345";
-		d.registerCustomer(id,username, password, name, surname, age, gender, address);
-		//The user shouldn't be able to register.Because id is not unique.
-		assertFalse(d.CheckCustomerUserName("bgoc")
-				&& d.CheckCustomerPassword("23456"));
+		//The user shouldn't be able to register.Because ids are not unique.
+		d.registerCustomer(1234,"bgoc","2345","baris","goc", 30,"Male","XXX");
 		d.registerEmployee(12,"kaan","nasvt","kaan","gokcek",42,"male","xxx");
-		//The user should be able to register.
-		assertFalse(d.CheckEmployeeUserName("kaan")&& d.CheckEmployeePassword("abcst"));
+		
+		assertFalse(d.CheckCustomerUserName("bgoc")&& d.CheckCustomerPassword("2345"));
+		assertFalse(d.CheckEmployeeUserName("kaan")&& d.CheckEmployeePassword("nasvt"));
 		assertFalse(d.CheckEmployeeUserName("bgoc")&& d.CheckEmployeePassword("2345"));
 		assertFalse(d.CheckCustomerUserName("kaan")&& d.CheckCustomerPassword("nasvt"));
 	}
--- src/test/testPasswordValidation.java
+++ src/test/testPasswordValidation.java
@@ -8,14 +8,18 @@ public class testPasswordValidation {
 	@Test	
 	public void testValidation1_P() {
 		String password1="1234Brs1234_";
+		String password2="_Brs_1234__";
 		//This password should satisfy conditions.
 	    assertTrue(password1.matches(strRegEx)?true:false);
+	    assertTrue(password2.matches(strRegEx)?true:false);
 	}
 	@Test
 	public void testValidation2_N() {
 		String password2="1234";
+		String password3="abcd";
 		//This password shouldn't satisfy conditions.
 		assertFalse(password2.matches(strRegEx)?true:false);
+		assertFalse(password3.matches(strRegEx)?true:false);
 	}
 	
 }
